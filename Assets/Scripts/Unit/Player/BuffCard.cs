07de2cd25c0faa10a5967ae657b6fc2422cc1fc0
MONGNOM <wjdahdgus123@gmail.com>
2024-01-26 00:24:46 +0900

    fix : BuffCard 수정

--- Assets/Scripts/Unit/Player/BuffCard.cs
+++ Assets/Scripts/Unit/Player/BuffCard.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 
 public class BuffCard : MonoBehaviour
 {
-    public List<Human> listhuman = new List<Human>();
-    public Human[] humans;
+   
     [SerializeField] float attackTime;
     [SerializeField] float damageTime;
-    Human human;
    
 
     
-    private void Start()
-    {
-        human = FindObjectOfType<Human>();
-        humans = FindObjectsOfType<Human>();
-        for (int i = 0; i < humans.Length; i++)
-        {
-            listhuman.Add(human);
-            listhuman[i] = humans[i];
-        }
-    }
+
     public void AttackSpeedUp()
     {
         StartCoroutine(SpeedUpTime());
@@ -29,9 +18,9 @@ public class BuffCard : MonoBehaviour
 
     public void Heal()
     {
-        for (int i = 0; i < listhuman.Count; i++)
+        for (int i = 0; i < WaveManager.instance.humans.Count; i++)
         {
-            listhuman[i].curhp += listhuman[i].curhp *= 0.5f;
+            WaveManager.instance.humans[i].curhp += WaveManager.instance.humans[i].curhp *= 0.5f;
         }
     }
 
@@ -42,33 +31,32 @@ public class BuffCard : MonoBehaviour
 
     IEnumerator SpeedUpTime()
     {
-        for (int i = 0; i < listhuman.Count; i++)
+        for (int i = 0; i < WaveManager.instance.humans.Count; i++)
         {
-            listhuman[i].anim.SetFloat("SalshSpeed", 2);
+            WaveManager.instance.humans[i].anim.SetFloat("SalshSpeed", 2);
         }
 
         yield return new WaitForSeconds(attackTime);
 
-        for (int i = 0; i < listhuman.Count; i++)
+        for (int i = 0; i < WaveManager.instance.humans.Count; i++)
         {
-            listhuman[i].anim.SetFloat("SalshSpeed", 1);
+            WaveManager.instance.humans[i].anim.SetFloat("SalshSpeed", 1);
         }
         StopCoroutine(SpeedUpTime());
-
     }
 
     IEnumerator DamageUpTime()
     {
-        for (int i = 0; i < listhuman.Count; i++)
+        for (int i = 0; i < WaveManager.instance.humans.Count; i++)
         {
-            listhuman[i].damage *= 1.2f;
+            WaveManager.instance.humans[i].damage *= 1.2f;
         }
 
         yield return new WaitForSeconds(damageTime);
 
-        for (int i = 0; i < listhuman.Count; i++)
+        for (int i = 0; i < WaveManager.instance.humans.Count; i++)
         {
-            listhuman[i].damage *= 0.85f;
+            WaveManager.instance.humans[i].damage *= 0.85f;
         }
         StopCoroutine(DamageUpTime());
     }
